b1267b4e6ae143aa70f7d18d4fa45d61e4a3f59b
Tehforsch <Tonipeter92@googlemail.com>
2020-06-11 21:30:05 +0200

    fix local configs for hdf5 problems

--- localConfigs/bwfor.py
+++ localConfigs/bwfor.py
@@ -3,7 +3,7 @@ from bob import config
 
 arepoDir = "/beegfs/home/hd/hd_hd/hd_hp240/projects/phd/arepo"
 
-jobParams = {"wallTime": "3:00:00", "jobName": "arepoTest", "logFile": "output.log", "maxCoresPerNode": 16}
+jobParams = {"wallTime": "3:00:00", "jobName": "arepoTest", "logFile": "stdout.log", "maxCoresPerNode": 16}
 
 runJobCommand = "msub"
 
@@ -12,12 +12,13 @@ jobTemplate = """ #MSUB -l nodes={numNodes}:ppn={coresPerNode}
 #MSUB -l pmem=4000mb
 #MSUB -N {jobName}
 #MSUB -o {logFile}
+#MSUB -v HDF5_DISABLE_VERSION_CHECK=2
 module load compiler/intel/16.0
-module load mpi/openmpi/2.1-intel-16.0
-module load lib/hdf5/1.8-intel-16.0
+module load mpi/impi/5.1.3-intel-16.0
 module load numlib/gsl/2.2.1-intel-16.0
 module load numlib/fftw/3.3.5-impi-5.1.3-intel-16.0
-
+module load lib/hdf5/1.8-intel-16.0
+module load devel/python_intel/3.6
 startexe="mpirun {runCommand}"
 cd $MOAB_SUBMITDIR
 exec $startexe """
--- localConfigs/local.py
+++ localConfigs/local.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 arepoDir = "/home/toni/projects/phd/arepo"
 
-jobParams = {"numCores": 1, "logFile": "output.log", "maxCoresPerNode": 2}
+jobParams = {"numCores": 1, "logFile": "stdout.log", "maxCoresPerNode": 2}
 runJobCommand = "bash"
 
 jobTemplate = """mpirun -n {numCores} {runCommand} | tee {logFile}"""
