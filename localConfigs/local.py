89c24f94e69f29f0fdbf4515fe4c5e4712c1f5a8
tehforsch <Tonipeter92@googlemail.com>
2021-02-25 11:53:59 +0100

    remove config-makefile for new version of arepo

--- localConfigs/local.py
+++ localConfigs/local.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-arepoDir = "/home/toni/projects/phd/arepo"
+arepoDir = "/home/toni/projects/arepo"
 
 jobParams = {"numCores": 2, "logFile": "stdout.log", "maxCoresPerNode": 2}
 runJobCommand = "bash"
