844f8f2efa7d7278f0c81c87c4e399ac69f00eef
tehforsch <Tonipeter92@googlemail.com>
2021-05-06 11:27:42 +0200

    adjust arepo folder in new bwfor structure

--- localConfigs/localBwfor.py
+++ localConfigs/localBwfor.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-arepoDir = "/beegfs/home/hd/hd_hd/hd_hp240/projects/phd/arepo"
+arepoDir = "/beegfs/home/hd/hd_hd/hd_hp240/projects/arepo"
 
 jobParams = {"numCores": 2, "logFile": "stdout.log", "maxCoresPerNode": 2}
 runJobCommand = "bash"
