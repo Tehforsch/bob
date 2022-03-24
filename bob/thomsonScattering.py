Fix calculation in thomson scattering for TNG sims

bob/thomsonScattering.py
@@ -18,26 +18,22 @@ def thomsonScattering(ax1: plt.axes, simSets: List[SimulationSet]) -> None:
     # dT, the time resolution, so time between steps in years
 
     for simSet in simSets:
-        ne = 1.0 * pq.cm ** (-3)
         snapshots = [(snap, sim) for sim in simSet for snap in sim.snapshots]
         snapshots.sort(key=lambda snapSim: -snapSim[0].time)
         redshifts = np.zeros(len(snapshots) - 1)
         dTau = np.zeros(len(snapshots) - 1)
         for (i, ((prevSnap, _), (snap, sim))) in enumerate(zip(snapshots, snapshots[1:])):
-            # xe = BasicField("ElectronAbundance").getData(snap)
-            # density = BasicField("Density").getData(snap) * snap.lengthUnit**(-3) * snap.massUnit
-            # ne = xe * density / protonMass
+            xe = BasicField("ElectronAbundance").getData(snap)
+            density = BasicField("Density").getData(snap) * snap.lengthUnit ** (-3) * snap.massUnit
+            ne = xe * density / protonMass
             dT = sim.getLookbackTime(snap.scale_factor) - sim.getLookbackTime(prevSnap.scale_factor)
-            print(dT.to(pq.s))
             redshifts[i] = sim.getRedshift(snap.scale_factor)
-            print(speedOfLight.decompose(), sig.decompose(), ne, np.mean(ne), dT)
-            print((speedOfLight))
-            print((speedOfLight * sig))
-            print((speedOfLight * sig * np.mean(ne)).decompose())
-            print((speedOfLight * sig * np.mean(ne) * dT))
             dTau[i] = speedOfLight * sig * np.mean(ne) * dT  # dTau at redshift index iz
 
         plt.plot(redshifts, np.cumsum(dTau))
+        plt.xlabel("z")
+        plt.ylabel("$\\tau$")
+        plt.xlim(5, 11)
 
         nx = int(50)
         ztau = np.linspace(0.0, 50.0, nx)