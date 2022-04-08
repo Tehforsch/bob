d54f133a3d497e66b9c66a5b8a3bc9bce3a057c9
tehforsch <mail@tonipeter.de>
2022-04-08 10:49:41 +0200

    change plot

--- bob/plots/temperatureDensityHistogram.py
+++ bob/plots/temperatureDensityHistogram.py
@@ -1,5 +1,6 @@
 import argparse
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 from bob.simulation import Simulation
@@ -14,12 +15,20 @@ class TemperatureDensityHistogram(SnapFn):
     def post(self, args: argparse.Namespace, sim: Simulation, snap: Snapshot) -> Result:
         temperature = Temperature().getData(snap)
         density = BasicField("Density").getData(snap)
+        print(
+                np.min(temperature),
+                np.mean(temperature),
+                np.max(temperature))
+        print(
+                np.min(density),
+                np.mean(density),
+                np.max(density))
         return Result([temperature, density])
 
     def plot(self, plt: plt.axes, result: Result) -> None:
         plt.xlabel("T")
         plt.ylabel("$\rho$")
-        plt.hist2d(result.arrs[0], result.arrs[1], [10, 10])
+        plt.hist2d(result.arrs[0], result.arrs[1], [5, 5], density=True)
         plt.colorbar()
 
 
