adjust units in basic field of density, fix running replot when no sims are present

bob/basicField.py
@@ -55,7 +55,11 @@ class BasicField(Field):
         return 1
 
     def getData(self, snapshot: "Snapshot") -> np.ndarray:
-        fieldData = readIntoNumpyArray(snapshot.hdf5File["PartType0"][self.name])
+        if self.name == "Density":
+            unit = snapshot.massUnit / (snapshot.lengthUnit**3)
+        else:
+            unit = 1
+        fieldData = readIntoNumpyArray(snapshot.hdf5File["PartType0"][self.name]) * unit
         if self.index is None:
             return fieldData
         else:

bob/plots/temperatureDensityHistogram.py
@@ -2,6 +2,7 @@ import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 from bob.simulation import Simulation
 from bob.snapshot import Snapshot
@@ -15,21 +16,32 @@ class TemperatureDensityHistogram(SnapFn):
     def post(self, args: argparse.Namespace, sim: Simulation, snap: Snapshot) -> Result:
         temperature = Temperature().getData(snap)
         density = BasicField("Density").getData(snap)
-        print(
-                np.min(temperature),
-                np.mean(temperature),
-                np.max(temperature))
-        print(
-                np.min(density),
-                np.mean(density),
-                np.max(density))
+        print(np.min(temperature), np.mean(temperature), np.max(temperature))
+        print(np.min(density), np.mean(density), np.max(density))
         return Result([temperature, density])
 
     def plot(self, plt: plt.axes, result: Result) -> None:
-        plt.xlabel("T")
-        plt.ylabel("$\rho$")
-        plt.hist2d(result.arrs[0], result.arrs[1], [5, 5], density=True)
-        plt.colorbar()
+        fig = plt.figure()
+        ax = fig.add_subplot(2, 1, 1)
+        plt.xlabel("T [K]")
+        plt.ylabel("$\\rho$")
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        # minX = np.min(result.arrs[0])
+        # maxX = np.max(result.arrs[0])
+        # minY = np.min(result.arrs[1])
+        # maxY = np.max(result.arrs[1])
+        minX = 1e0
+        maxX = 1e6
+        minY = 1e-10
+        maxY = 1e-4
+        binsX = np.logspace(np.log10(minX), np.log10(maxX), num=104)
+        binsY = np.logspace(np.log10(minY), np.log10(maxY), num=104)
+        plt.xticks([1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6])
+        plt.yticks([1e-10, 1e-8, 1e-6, 1e-4])
+        plt.hist2d(result.arrs[0], result.arrs[1], [binsX, binsY], density=True, norm=colors.LogNorm())
+        colorbar = plt.colorbar()
+        colorbar.set_ticks([1e-5, 1e-0, 1e5])
 
 
 addToList("temperatureDensityHistogram", TemperatureDensityHistogram())

bob/plots/timePlots.py
@@ -35,7 +35,7 @@ def getTimeQuantityForSnap(quantity: str, sim: Simulation, snap: Snapshot) -> fl
 class TimePlot(MultiSetFn):
     def init(self, args: argparse.Namespace) -> None:
         super().init(args)
-        self.styles: List[Dict[str, Any]] = [{}]
+        self.styles: List[Dict[str, Any]] = [{}, {}, {}]
 
     @abstractmethod
     def getQuantity(self, args: argparse.Namespace, sims: Simulation, snap: Snapshot) -> float:
@@ -59,8 +59,10 @@ class TimePlot(MultiSetFn):
     def plot(self, plt: plt.axes, result: Result) -> None:
         plt.xlabel(self.xlabel())
         plt.ylabel(self.ylabel())
+        self.labels = ["SWEEP", "TNG"]
         for (style, label, arr) in itertools.zip_longest(self.styles, self.labels, result.arrs):
-            plt.plot(arr[0, :], arr[1, :], label=label, **style)
+            print(style, label, arr)
+            plt.plot(arr[0, :], arr[1, :], label=label)
         plt.legend()
 
     def getQuantityOverTime(self, args: argparse.Namespace, simSet: SimulationSet) -> np.ndarray: