7c2b7f6148d2781ca6b567bddc526d1c23ad0d70
tehforsch <mail@tonipeter.de>
2022-04-12 15:34:32 +0200

    adjust ticks in temperature density histogram

--- bob/plots/temperatureDensityHistogram.py
+++ bob/plots/temperatureDensityHistogram.py
@@ -24,24 +24,24 @@ class TemperatureDensityHistogram(SnapFn):
         fig = plt.figure()
         ax = fig.add_subplot(2, 1, 1)
         plt.xlabel("T [K]")
-        plt.ylabel("$\\rho$")
+        plt.ylabel("$\\rho [\mathrm{g} / \mathrm{cm}^3]$")
         ax.set_xscale("log")
         ax.set_yscale("log")
         # minX = np.min(result.arrs[0])
         # maxX = np.max(result.arrs[0])
         # minY = np.min(result.arrs[1])
         # maxY = np.max(result.arrs[1])
-        minX = 1e0
-        maxX = 1e6
-        minY = 1e-10
-        maxY = 1e-4
+        minX = 5e0
+        maxX = 2e6
+        minY = 1e-31
+        maxY = 5e-26
         binsX = np.logspace(np.log10(minX), np.log10(maxX), num=104)
         binsY = np.logspace(np.log10(minY), np.log10(maxY), num=104)
-        plt.xticks([1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6])
-        plt.yticks([1e-10, 1e-8, 1e-6, 1e-4])
+        plt.xticks([1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7])
+        plt.yticks([1e-31, 1e-30, 1e-29, 1e-28, 1e-27, 1e-26])
         plt.hist2d(result.arrs[0], result.arrs[1], [binsX, binsY], density=True, norm=colors.LogNorm())
         colorbar = plt.colorbar()
-        colorbar.set_ticks([1e-5, 1e-0, 1e5])
+        colorbar.set_ticks([1e17, 1e22, 1e27])
 
 
 addToList("temperatureDensityHistogram", TemperatureDensityHistogram())
