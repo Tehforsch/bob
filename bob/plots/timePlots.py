aa3241b6e2493b621de0e0b4350deb14a5fa6932
tehforsch <mail@tonipeter.de>
2022-04-19 16:54:39 +0200

    add constraints to temperature plot

--- bob/plots/timePlots.py
+++ bob/plots/timePlots.py
@@ -98,5 +98,39 @@ class MeanFieldOverTime(TimePlot):
         data = self.field.getData(snap) / self.field.unit
         return np.mean(data)
 
+    def plot(self, plt: plt.axes, result: Result) -> None:
+        if self.field.niceName == "Temperature":
+            self.addConstraints(plt)
+        super().plot(plt, result)
+
+    def addConstraints(self, ax: plt.axes) -> None:
+        boera19_temps = np.asarray([[4.6, 7.31e3, 0.88e3, 1.35e3], [5.0, 7.37e3, 1.39e3, 1.67e3]])
+
+        walther19_zeds = np.asarray([4.6, 5.0, 5.4])
+        walther19_temps = np.asarray([[0.877e4, 0.106e4, 0.130e4], [0.533e4, 0.091e4, 0.122e4], [0.599e4, 0.134e4, 0.152e4]])
+        ax.errorbar(
+            boera19_temps[:, 0],
+            boera19_temps[:, 1],
+            yerr=boera19_temps[:, 2:],
+            label="Boera+19",
+            c="k",
+            ls="none",
+            marker="D",
+            capsize=3,
+            elinewidth=1,
+        )
+
+        ax.errorbar(
+            walther19_zeds - 0.01,
+            walther19_temps[:, 0],
+            yerr=walther19_temps[:, 1:].T,
+            label="Walther+19",
+            c="k",
+            ls="none",
+            marker="o",
+            capsize=3,
+            elinewidth=1,
+        )
+
 
 addToList("meanFieldOverTime", MeanFieldOverTime())
