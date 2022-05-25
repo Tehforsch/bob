062e85502bbf9219d614ed93444db6ab18e7e46d
tehforsch <mail@tonipeter.de>
2022-05-25 20:02:30 +0200

    fix getArrayQuantity

--- bob/util.py
+++ bob/util.py
@@ -1,6 +1,7 @@
 from typing import List
 import itertools
 
+import unittest
 from pathlib import Path
 import numpy as np
 import astropy.units as pq
@@ -14,5 +15,24 @@ def getCommonParentFolder(folders: List[Path]) -> Path:
 
 
 def getArrayQuantity(quantities: List[pq.Quantity]) -> pq.Quantity:
-    unit = quantities[0].unit
-    return np.array((y / unit).decompose().value for y in quantities) * unit
+    unit = 1.0 * quantities[0].unit
+    for y in quantities:
+        if not (y / unit).decompose().unit == "":
+            raise ValueError("Different units in quantity array")
+    return np.array([(y / unit).decompose().value for y in quantities]) * unit
+
+class Test(unittest.TestCase):
+    def test_get_array_quantity(self) -> None:
+        a = [1.0 * pq.m, 1.0 * pq.cm]
+        result = getArrayQuantity(a)
+        assert (result.value == np.array([1.0, 0.01])).all()
+        assert result.unit == pq.m
+
+    def test_array_quantity_fails_if_given_different_units(self) -> None:
+        # try:
+        a = [1.0 * pq.m, 1.0 * pq.J]
+        try:
+            result = getArrayQuantity(a)
+            assert False
+        except ValueError:
+            pass
