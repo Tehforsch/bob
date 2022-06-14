4f4afe79e3059b086c7c5de2d30f10c75dc4c5d1
tehforsch <mail@tonipeter.de>
2022-06-14 12:48:58 +0200

    fix ionization time plot

--- bob/plots/ionizationTime.py
+++ bob/plots/ionizationTime.py
@@ -40,30 +40,34 @@ class IonizationTime(SetFn):
         axis = np.array([0.0, 0.0, 1.0])
         n1 = config.dpi * 3
         n2 = config.dpi * 3
-        data = np.zeros((n1, n2))
         ionizationTime = None
         for sim in simSet:
+            if len(sim.snapshots) == 0:
+                print("No snapshots in sim? Continuing.")
+                continue
             snap = max(sim.snapshots, key=lambda snap: getTimeQuantityForSnap(self.quantity, sim, snap))
-            newIonizationTime = BasicField("IonizationTime").getData(snap)
-            newIonizationTime = getTimeQuantityFromTimeOrScaleFactor(self.quantity, sim, snap, ionizationTime / snap.timeUnit)
+            center = snap.center
+            ortho1, ortho2 = findOrthogonalAxes(axis)
+            min1 = np.dot(ortho1, snap.minExtent)
+            min2 = np.dot(ortho2, snap.minExtent)
+            max1 = np.dot(ortho1, snap.maxExtent)
+            max2 = np.dot(ortho2, snap.maxExtent)
+            p1, p2 = np.meshgrid(np.linspace(min1, max1, n1), np.linspace(min2, max2, n2))
+            coordinates = axis * (center * axis) + np.outer(p1, ortho1) + np.outer(p2, ortho2)
+            tree = cKDTree(snap.coordinates)
+            cellIndices = tree.query(coordinates)[1]
+            cellIndices = cellIndices.reshape((n1, n2))
+            newIonizationTime = BasicField("IonizationTime").getData(snap)[cellIndices]
+            newIonizationTime = getTimeQuantityFromTimeOrScaleFactor(self.quantity, sim, snap, newIonizationTime / snap.timeUnit)
             if ionizationTime is None:
                 ionizationTime = newIonizationTime
             else:
-                ionizationTime = np.min(ionizationTime, newIonizationTime)
-        center = snap.center
-        ortho1, ortho2 = findOrthogonalAxes(axis)
-        min1 = np.dot(ortho1, snap.minExtent)
-        min2 = np.dot(ortho2, snap.minExtent)
-        max1 = np.dot(ortho1, snap.maxExtent)
-        max2 = np.dot(ortho2, snap.maxExtent)
-        p1, p2 = np.meshgrid(np.linspace(min1, max1, n1), np.linspace(min2, max2, n2))
-        coordinates = axis * (center * axis) + np.outer(p1, ortho1) + np.outer(p2, ortho2)
-        tree = cKDTree(snap.coordinates)
-        cellIndices = tree.query(coordinates)[1]
-        cellIndices = cellIndices.reshape((n1, n2))
+                unit = ionizationTime.unit
+                value1 = ionizationTime.to(unit).value
+                value2 = newIonizationTime.to(unit).value
+                ionizationTime = np.minimum(value1, value2) * unit
         if ionizationTime is not None:
-            data = ionizationTime[cellIndices]
-            return (min1, min2, max1, max2), data
+            return (min1, min2, max1, max2), ionizationTime
         else:
             raise ValueError("No sims/snaps")
 
