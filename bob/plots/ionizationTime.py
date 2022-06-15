092bb49dc46ea837889ae52cdf0e432d8029b67a
tehforsch <mail@tonipeter.de>
2022-06-15 14:18:54 +0200

    adjust defaults

--- bob/plots/ionizationTime.py
+++ bob/plots/ionizationTime.py
@@ -21,11 +21,11 @@ class IonizationTime(SetFn):
         return result
 
     def plot(self, plt: plt.axes, result: Result) -> None:
-        self.style.setDefault("xLabel", "$x [h^{-1} \mathrm{Mpc}]$")
-        self.style.setDefault("yLabel", "$y [h^{-1} \mathrm{Mpc}]$")
         self.style.setDefault("cLabel", "$t [\mathrm{Myr}]$")
-        self.style.setDefault("xUnit", pq.Gpc)
-        self.style.setDefault("yUnit", pq.Gpc)
+        self.style.setDefault("xUnit", pq.Mpc)
+        self.style.setDefault("yUnit", pq.Mpc)
+        self.style.setDefault("xLabel", "$x [h^{-1} \mathrm{UNIT}]$")
+        self.style.setDefault("yLabel", "$y [h^{-1} \mathrm{UNIT}]$")
         self.style.setDefault("vUnit", pq.Myr)
         self.style.setDefault("vLim", (0.0, 2e2))
         self.setupLabels()
