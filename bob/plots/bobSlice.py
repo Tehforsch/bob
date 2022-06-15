fix slice plot using length units instead of code units

bob/plots/bobSlice.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from scipy.spatial import cKDTree
 import numpy as np
+import astropy.units as pq
 
 from bob.simulation import Simulation
 from bob.snapshot import Snapshot
@@ -16,37 +17,40 @@ from bob.field import Field
 
 
 def getDataAtPoints(field: Field, snapshot: Snapshot, points: np.ndarray) -> np.ndarray:
-    tree = cKDTree(snapshot.coordinates)
+    tree = cKDTree(snapshot.coordinates.to(snapshot.lengthUnit).value)
     cellIndices = tree.query(points)[1]
     data = field.getData(snapshot)
     return data[cellIndices]
 
-
 class VoronoiSlice(SnapFn):
-    def post(self, args: argparse.Namespace, sim: Simulation, snap: Snapshot) -> Result:
-        axis = getAxisByName(args.axis)
-        field = getFieldByName(args.field)
+    def getSlice(self, field: Field, snapshot: Snapshot, axisName: str) -> pq.Quantity:
+        axis = getAxisByName(axisName)
         axis = np.array(axis)
-        center = snap.center
-        self.ortho1, self.ortho2 = findOrthogonalAxes(axis)
-        self.min1 = np.dot(self.ortho1, snap.minExtent)
-        self.min2 = np.dot(self.ortho2, snap.minExtent)
-        self.max1 = np.dot(self.ortho1, snap.maxExtent)
-        self.max2 = np.dot(self.ortho2, snap.maxExtent)
+        center = snapshot.center
+        ortho1, ortho2 = findOrthogonalAxes(axis)
+        self.min1 = np.dot(ortho1, snapshot.minExtent)
+        self.min2 = np.dot(ortho2, snapshot.minExtent)
+        self.max1 = np.dot(ortho1, snapshot.maxExtent)
+        self.max2 = np.dot(ortho2, snapshot.maxExtent)
         n1 = config.dpi * 3
         n2 = config.dpi * 3
         p1, p2 = np.meshgrid(np.linspace(self.min1, self.max1, n1), np.linspace(self.min2, self.max2, n2))
-        coordinates = axis * (center * axis) + np.outer(p1, self.ortho1) + np.outer(p2, self.ortho2)
+        coordinates = axis * (center * axis) + np.outer(p1, ortho1) + np.outer(p2, ortho2)
+        data = getDataAtPoints(field, snapshot, coordinates)
+        return data.reshape((n1, n2))
+
+    def post(self, args: argparse.Namespace, sim: Simulation, snap: Snapshot) -> Result:
+        self.axis = args.axis
+        field = getFieldByName(args.field)
         result = Result()
-        result.data = getDataAtPoints(field, snap, coordinates)
-        result.data = result.data.reshape((n1, n2))
-        print(result.data.shape)
+        result.data = self.getSlice(field, snap, args.axis)
         print(f"Field: {field.niceName}: min: {np.min(result.data):.2e}, mean: {np.mean(result.data):.2e}, max: {np.max(result.data):.2e}")
         return result
 
     def plot(self, plt: plt.axes, result: Result) -> None:
-        plt.xlabel(getAxisName(self.ortho1))
-        plt.ylabel(getAxisName(self.ortho2))
+        xAxis, yAxis = getOtherAxes(self.axis)
+        plt.xlabel(xAxis)
+        plt.ylabel(yAxis)
         extent = (self.min1, self.max1, self.min2, self.max2)
         vmin = 1.0e2
         vmax = 1.0e5
@@ -74,6 +78,13 @@ def getAxisName(axis: np.ndarray) -> str:
     mostParallel = np.argmax(dotProducts)
     return ["x", "y", "z"][mostParallel]
 
+def getOtherAxes(axis: str) -> str:
+    if axis == "x":
+        return "y", "z"
+    elif axis == "y":
+        return "x", "z"
+    elif axis == "z":
+        return "x", "y"
 
 def findOrthogonalAxes(axis: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     xAxis = np.array([1.0, 0.0, 0.0])