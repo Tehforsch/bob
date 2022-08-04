8f921f951854d5414b46ecd9ee78ea389d9b269e
tehforsch <mail@tonipeter.de>
2022-08-04 16:49:36 +0200

    add ionization rate constraints

--- bob/plots/ionizationRate.py
+++ bob/plots/ionizationRate.py
@@ -8,6 +8,9 @@ from bob.multiSet import MultiSet
 from bob.plots.ionization import IonizationData
 
 
+from astropy.io import ascii
+
+
 class IonizationRate(MultiSetFn):
     def post(self, args: argparse.Namespace, simSets: MultiSet) -> Result:
         result = IonizationData(simSets)
@@ -18,7 +21,8 @@ class IonizationRate(MultiSetFn):
         self.style.setDefault("yLabel", "R")
         self.style.setDefault("xUnit", pq.dimensionless_unscaled)
         self.style.setDefault("yUnit", 1 / pq.s)
-        self.style.setDefault("legend_loc", "lower left")
+        self.style.setDefault("legend_loc", "upper right")
+        self.style.setDefault("yLim", [1e-18, 1e-12])
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
         ax.set_yscale("log")
@@ -30,7 +34,37 @@ class IonizationRate(MultiSetFn):
             self.addLine(redshift, massRate, color=color, linestyle="--", label="")
         plt.plot([], [], color="black", linestyle="-", label="volume av.")
         plt.plot([], [], color="black", linestyle="--", label="mass av.")
+        self.addConstraints()
         plt.legend(loc=self.style["legend_loc"])
+
+    def addConstraints(self) -> None:
+        data = ascii.read(".obs/faucher-giguere2008/table1")
+        [zfc08, tauafc08, taubfc08, gamfc08, e_gamfc08] = data.to_pandas().values.T
+        print(zfc08)
+        # lgamfc08 = gamfc08 * 1e-12
+        print(e_gamfc08)
+        print(gamfc08)
+
+        # data = ascii.read("./obs/becker_bolton2013/table1")
+        # d=data.to_pandas()
+        # d=d.values
+        # [zbb13,lgambb13,erplgbb13,ermlgbb13]=d
+
+        # data = ascii.read("./obs/daloisio2018/table2")
+        # d=data.to_pandas()
+        # d=d.values
+        # [zdal18,gamdal18,erpdal18,ermdal18]=d
+        # lgamdal18=np.log10(gamdal18)
+        # erpldal18=np.log10(gamdal18+erpdal18)-np.log10(gamdal18)
+        # ermldal18=(np.log10(gamdal18-ermdal18)-np.log10(gamdal18))
+
+        # ax.errorbar(zcal11,lgammacal11,yerr=erlgammacal11,fmt='o',color='r',mec='r',
+        #             label=r"Calverley+11",capsize=5)
+        plt.errorbar(zfc08, 1e-12 * gamfc08, yerr=1e-12 * e_gamfc08, fmt="s", color="g", mec="g", label=r"Faucher-Giguere+08", capsize=5)
+        # ax.errorbar(zbb13,lgambb13-12.,yerr=[erplgbb13,-ermlgbb13],fmt='^',color='b',mec='b',
+        #             label=r"Becker-Bolton+13",capsize=5)
+        # ax.errorbar(zdal18,lgamdal18-12.,yerr=[erpldal18,-ermldal18],fmt='*',color='m',mec='m',
+        #             label=r"D'Aloisio+18",capsize=5)
 
 
 addToList("ionizationRate", IonizationRate())
