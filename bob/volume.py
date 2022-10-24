cdbfa20f1eafa3834c1322a6421f726188d90184
tehforsch <mail@tonipeter.de>
2022-10-24 12:56:13 +0200

    add volume

---
+++ bob/volume.py
@@ -0,0 +1,31 @@
+import numpy as np
+from bob.constants import kB, protonMass, gamma
+from bob.field import Field
+from bob.basicField import BasicField
+from bob.snapshot import Snapshot
+import astropy.units as pq
+
+
+class Volume(Field):
+    def __init__(self, comoving: bool) -> None:
+        self.comoving = comoving
+        
+    def getData(self, snapshot: Snapshot) -> np.ndarray:
+        density = BasicField("Density").getData(snapshot)
+        masses = BasicField("Masses").getData(snapshot)
+        return masses / density
+
+    @property
+    def niceName(self) -> str:
+        return "Volume"
+
+    @property
+    def symbol(self) -> str:
+        return "V"
+
+    @property
+    def unit(self) -> pq.Quantity:
+        if self.comoving:
+            return pq.cm**3 / pq.h**3
+        else:
+            return pq.cm**3
