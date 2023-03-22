34058afd11574ee78a5db32666908fcfda01baa6
tehforsch <mail@tonipeter.de>
2023-03-22 11:30:33 +0100

    fix h2 plot

--- bob/plots/h2Expansion.py
+++ bob/plots/h2Expansion.py
@@ -4,11 +4,8 @@ import astropy.units as pq
 
 from bob.simulation import Simulation
 from bob.snapshot import Snapshot
-from bob import config
 from bob.postprocessingFunctions import SnapFn
 from bob.result import Result
-from bob.allFields import allFields, getFieldByName
-from bob.field import Field
 from bob.plotConfig import PlotConfig
 from bob.fieldOverRadius import getDataForRadii
 from bob.basicField import BasicField
@@ -17,10 +14,10 @@ from bob.temperature import Temperature
 class H2Expansion(SnapFn):
     def __init__(self, config: PlotConfig) -> None:
         super().__init__(config)
-        self.config.setDefault("num", 100)
+        self.config.setDefault("num", 20)
         self.config.setDefault("xLabel", "r [UNIT]")
         self.config.setDefault("yLabel", "F [UNIT]")
-        self.config.setDefault("xUnit", "kpc")
+        self.config.setDefault("xUnit", "pc")
         self.config.setDefault("yUnit", "1 / s")
 
     def post(self, sim: Simulation, snap: Snapshot) -> Result:
@@ -28,16 +25,16 @@ class H2Expansion(SnapFn):
         boxSize = sim.params["BoxSize"] / 2.0
         lengthUnit = sim.params["UnitLength_in_cm"] * pq.cm
         result.radii = np.linspace(0, boxSize, num=self.config["num"]) * lengthUnit
-        coordinates = BasicField("Coordinates").getData(snap) 
-        center = np.array([1,1,1]) * boxSize * lengthUnit
+        coordinates = BasicField("Coordinates").getData(snap)
+        center = np.array([1, 1, 1]) * boxSize * lengthUnit
         abundances = BasicField("ChemicalAbundances").getData(snap)
         fluxes = BasicField("PhotonFlux").getData(snap)
         temperature = Temperature().getData(snap)
-        fields = [abundances[:,0], abundances[:,1], temperature, fluxes[:, 0], fluxes[:, 1], fluxes[:, 2]]
-        data = []
-        
+
         result.ab0 = getDataForRadii(abundances[:, 0], center, coordinates, result.radii)
         result.ab1 = getDataForRadii(abundances[:, 1], center, coordinates, result.radii)
+        ionizationFront = np.argmax(result.ab1 < 0.25)
+        result.ionizationFrontRadius = result.radii[ionizationFront]
         result.temperature = getDataForRadii(temperature, center, coordinates, result.radii)
         result.flux0 = getDataForRadii(fluxes[:, 0], center, coordinates, result.radii, mean=False)
         result.flux1 = getDataForRadii(fluxes[:, 1], center, coordinates, result.radii, mean=False)
@@ -48,18 +45,20 @@ class H2Expansion(SnapFn):
         fig, axes  = plt.subplots(2, 3, figsize=(12,6))
         [[ax00, ax01, ax02], [ax10, ax11, ax12]] = axes
         self.setupLabels()
-        ax00.plot(result.radii.to(pq.kpc), result.ab0)
-        ax01.plot(result.radii.to(pq.kpc), result.ab1)
-        ax02.plot(result.radii.to(pq.kpc), result.temperature.to_value(pq.K))
-        ax10.plot(result.radii.to(pq.kpc), result.flux1)
-        ax11.plot(result.radii.to(pq.kpc), result.flux2)
-        ax12.plot(result.radii.to(pq.kpc), result.flux0)
+        ax00.plot(result.radii.to(pq.pc), result.ab0)
+        ax01.plot(result.radii.to(pq.pc), result.ab1)
+        ax02.plot(result.radii.to(pq.pc), result.temperature.to_value(pq.K))
+        ax10.plot(result.radii.to(pq.pc), result.flux1)
+        ax11.plot(result.radii.to(pq.pc), result.flux2)
+        ax12.plot(result.radii.to(pq.pc), result.flux0)
         fluxes = [result.flux1, result.flux2, result.flux0]
         for iax, ax in enumerate([ax10, ax11, ax12]):
             ax.set_yscale("log")
-            ax.set_ylim( np.max(fluxes[iax]).to_value(1 / pq.s)/1e2,np.max(fluxes[iax]).to_value(1 / pq.s)*5)
+            max_flux = np.max(fluxes[iax]).to_value(1 / pq.s)
+            ax.set_ylim(max_flux / 5e2, max_flux * 1.01)
         for ax in axes[1]:
-            ax.set_xlabel("r [kpc]")
+            ax.set_xlabel("r [pc]")
+            ax.axvline(result.ionizationFrontRadius.to_value(pq.pc), c="red", linestyle="-")
         ax00.set_ylabel("H_2")
         ax01.set_ylabel("H+")
         ax02.set_ylabel("T")
