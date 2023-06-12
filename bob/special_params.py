e3625da1d7634391a47562aa40302fab69da22a5
tehforsch <mail@tonipeter.de>
2023-06-12 20:49:56 +0200

    add special param

--- bob/special_params.py
+++ bob/special_params.py
@@ -20,3 +20,13 @@ class ReadGrid(yaml.YAMLObject):
     @classmethod
     def from_yaml(cls, loader, node):
         return cls(node.value)
+
+class ConstructGrid(yaml.YAMLObject):
+    yaml_tag = "!construct"
+
+    def __init__(self, val):
+        self.val = val
+
+    @classmethod
+    def from_yaml(cls, loader, node):
+        return cls(node.value)
