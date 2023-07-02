fix cosmological unit readin by being very specific about which units are ok

bob/raxiomSnapshot.py
@@ -1,5 +1,6 @@
 import re
 import astropy.units as pq
+import astropy.cosmology.units as cu
 from pathlib import Path
 from bob.baseSnapshot import BaseSnapshot
 import os
@@ -7,6 +8,8 @@ from bob.config import (
     LENGTH_SCALING_IDENTIFIER,
     TIME_SCALING_IDENTIFIER,
     MASS_SCALING_IDENTIFIER,
+    A_SCALING_IDENTIFIER,
+    H_SCALING_IDENTIFIER,
     SCALE_FACTOR_SI_IDENTIFIER,
     SNAPSHOT_FILE_NAME,
     TEMPERATURE_SCALING_IDENTIFIER,
@@ -14,6 +17,7 @@ from bob.config import (
 from bob.util import getFolders, getFiles, printOnce
 import h5py
 import numpy as np
+from bob.config import setupAstropy
 
 
 class SnapshotFileInfo:
@@ -37,7 +41,7 @@ class RaxiomSnapshot(BaseSnapshot):
         self.num = snapshot_infos[0].num
         assert all(f.num == self.num for f in snapshot_infos)
         self.lengthUnit = pq.m
-        self.H0 = 65.0 * (pq.m / pq.s / pq.Mpc)
+        self.H0 = 65.0 * (pq.km / pq.s / pq.Mpc)
         printOnce("Returning fake value for H0")
 
     def hdf5FilesWithDataset(self, dataset: str) -> list[h5py.File]:
@@ -91,10 +95,23 @@ class RaxiomSnapshot(BaseSnapshot):
         unit = read_unit_from_dataset(name, files[0])
         return unit * data
 
+    def scale_factor(self) -> pq.Quantity:
+        if "cosmology" in self.sim.params:
+            return self.sim.params["cosmology"]["a"] * pq.dimensionless_unscaled
+        else:
+            return 1.0
+
     @property
     def maxExtent(self):
         try:
-            return np.array([1.0, 1.0, 1.0]) * pq.Quantity(self.sim.params["box_size"])
+            a = pq.def_unit("a", self.scale_factor().value * pq.dimensionless_unscaled)
+            pq.add_enabled_units([a])
+            boxSize = pq.Quantity(self.sim.params["box_size"])
+            boxSize = boxSize.to(self.lengthUnit, cu.with_H0(self.H0))
+            setupAstropy()
+            # reset the units
+
+            return np.array([1.0, 1.0, 1.0]) * boxSize
         except ValueError:
             raise NotImplementedError
 
@@ -113,6 +130,10 @@ def read_unit_from_dataset(dataset_name: str, f: h5py.File) -> pq.Quantity:
     unit *= pq.s ** dataset.attrs[TIME_SCALING_IDENTIFIER]
     unit *= pq.kg ** dataset.attrs[MASS_SCALING_IDENTIFIER]
     unit *= pq.K ** dataset.attrs[TEMPERATURE_SCALING_IDENTIFIER]
+    if dataset.attrs[A_SCALING_IDENTIFIER] != 0:
+        raise NotImplementedError
+    if dataset.attrs[H_SCALING_IDENTIFIER] != 0:
+        raise NotImplementedError
     return unit * dataset.attrs[SCALE_FACTOR_SI_IDENTIFIER]
 
 