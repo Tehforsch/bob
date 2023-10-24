80544aae483354b39025a56a4b3ee68063c07659
tehforsch <mail@tonipeter.de>
2023-10-24 10:38:33 +0200

    update sourcefield plot

--- bob/sourceField.py
+++ bob/sourceField.py
@@ -1,6 +1,7 @@
 from bob.field import Field
 from bob.basicField import BasicField
 from bob.snapshot import Snapshot
+from bob.subsweepSnapshot import SubsweepSnapshot
 
 import numpy as np
 import astropy.units as pq
@@ -13,25 +14,28 @@ class SourceField(Field):
         coords = BasicField("Coordinates", partType=0).getData(snapshot)
         tree = cKDTree(coords)
         source = np.zeros(coords.shape[0]) / pq.s
-        type_ = snapshot.sim.params["SX_SOURCES"]
-        if type_ == 4:
-            try:
-                starMasses = BasicField("Masses", partType=4).getData(snapshot)
-                starCoords = BasicField("Coordinates", partType=4).getData(snapshot)
-            except KeyError:
-                # Might just be that there are no stars at this point
-                return source
-            cellIndices = tree.query(starCoords)[1]
-            print("Using ridiculous approximation for source strength. To do this properly, write out stellar age and take SourceFactor into account")
-            source[cellIndices] = 1e40 / pq.s * starMasses.to_value(pq.Msun, cu.with_H0(snapshot.H0))
-        elif type_ == 10:
-            sources = snapshot.sim.sources()
-            starCoords = sources.getCoords(snapshot.sim)
-            cellIndices = tree.query(starCoords)[1]
-            luminosity = sources.get136IonisationRate(snapshot.sim)
-            source[cellIndices] = luminosity
+        if type(snapshot) == SubsweepSnapshot:
+            source = snapshot.source()
         else:
-            raise NotImplementedError("currently, source field is only implemented for star particles and test sources")
+            type_ = snapshot.sim.params["SX_SOURCES"]
+            if type_ == 4:
+                try:
+                    starMasses = BasicField("Masses", partType=4).getData(snapshot)
+                    starCoords = BasicField("Coordinates", partType=4).getData(snapshot)
+                except KeyError:
+                    # Might just be that there are no stars at this point
+                    return source
+                cellIndices = tree.query(starCoords)[1]
+                print("Using ridiculous approximation for source strength. To do this properly, write out stellar age and take SourceFactor into account")
+                source[cellIndices] = 1e40 / pq.s * starMasses.to_value(pq.Msun, cu.with_H0(snapshot.H0))
+            elif type_ == 10:
+                sources = snapshot.sim.sources()
+                starCoords = sources.getCoords(snapshot.sim)
+                cellIndices = tree.query(starCoords)[1]
+                luminosity = sources.get136IonisationRate(snapshot.sim)
+                source[cellIndices] = luminosity
+            else:
+                raise NotImplementedError("currently, source field is only implemented for star particles and test sources")
         return source
 
     @property
