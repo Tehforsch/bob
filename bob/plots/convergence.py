378241da0c605f268972b8098842df589080cd13
tehforsch <mail@tonipeter.de>
2023-11-03 12:34:22 +0100

    update

--- bob/plots/convergence.py
+++ bob/plots/convergence.py
@@ -23,14 +23,13 @@ class Convergence(MultiSetFn):
 
     def post(self, simSets: MultiSet) -> Result:
         convergedFraction = 0.4713
-        ERROR_THRESHOLD = 0.01
+        error_thresholds = [0.1, 0.01, 0.001, 0.0001]
         dfs = []
         for sims in simSets:
             for sim in sims:
                 print(sim.folder)
                 data = sim.get_timeseries("hydrogen_ionization_mass_average")
                 finalFraction = data.value[-1]
-                converged = abs(finalFraction - convergedFraction) < ERROR_THRESHOLD
                 num_particles = sim.params["1d"]["num_particles"]
                 flux = pq.Quantity(sim.params["1d"]["photon_flux"])
                 number_density = pq.Quantity(sim.params["1d"]["number_density"])
@@ -38,30 +37,31 @@ class Convergence(MultiSetFn):
                 length_cell = length_box / num_particles
                 timescale = length_cell * number_density / flux
                 runtime = sim.get_performance_data()["Stages::Sweep"]["total"]
-                subdf = pl.DataFrame(
-                    {
+                entries = {
                         "dt[kyr]": pq.Quantity(sim.params["sweep"]["max_timestep"]).to_value(pq.kyr),
                         "num_levels": sim.params["sweep"]["num_timestep_levels"],
                         "num_particles": num_particles,
                         "threshold": pq.Quantity(sim.params["sweep"]["significant_rate_threshold"]).value,
                         "timescale[kyr]": timescale.to_value(pq.kyr),
-                        "converged": converged,
                         "ratio": timescale.to_value(pq.kyr) / pq.Quantity(sim.params["sweep"]["max_timestep"]).to_value(pq.kyr),
                         "runtime[s]": pq.Quantity(runtime).to_value(pq.s),
                     }
-                )
+                for (i, threshold) in enumerate(error_thresholds):
+                    converged = abs(finalFraction - convergedFraction) < threshold
+                    entries["converged_{i}"] = converged
+                subdf = pl.DataFrame(entries)
                 dfs.append(subdf)
         df = pl.concat(dfs)
+        return df
+
+    def plot(self, plt: plt.axes, df: Result) -> None:
+        plt.clf()
+        df = df.rename({"num_levels": "n"})
         df = df.filter(pl.col("converged") == 1)
         dfs = df.partition_by(["num_levels", "num_particles", "threshold"])
         dfs = [df.top_k(1, by="dt[kyr]") for df in dfs]
         df = pl.concat(dfs)
-        return df
-
-    def plot(self, plt: plt.axes, result: Result) -> None:
-        plt.clf()
-        result = result.rename({"num_levels": "n"})
-        print(result)
+        print(df)
 
         fig=plt.figure()
         ax1 = plt.subplot(211)
@@ -72,7 +72,7 @@ class Convergence(MultiSetFn):
 
         ax = sns.lineplot(
             ax=ax1,
-            data=result,
+            data=df,
             x="num_particles",
             y="dt[kyr]",
             hue="n",
@@ -84,7 +84,7 @@ class Convergence(MultiSetFn):
 
         ax = sns.lineplot(
             ax=ax2,
-            data=result,
+            data=df,
             x="num_particles",
             y="runtime[s]",
             hue="n",
