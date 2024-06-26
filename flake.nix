a2b1ae57596ccbf8a43deded475e85d6a1f6e33f
tehforsch <mail@tonipeter.de>
2024-06-26 18:06:53 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+# For now, I have rustup in my path anyways, so this is more an
+# illustration how to do this in general.
+{
+  description = "My dev shells";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    overlays = [ (import rust-overlay) ];
+    pkgs = import nixpkgs { inherit system overlays; };
+  in {
+    devShells.python = with pkgs;
+    mkShell {
+      buildInputs = [
+        texliveSmall
+        (python3.withPackages (p: with p; [
+          numpy
+          pyyaml
+          seaborn
+          matplotlib
+          astropy
+          h5py
+          polars
+          scipy
+          tqdm
+          scikit-learn
+        ]))
+      ];
+    };
+  });
+}
